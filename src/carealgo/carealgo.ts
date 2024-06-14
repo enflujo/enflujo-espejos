c441181bfc8157a17803dbe879c577d28fc93526
anattolia <anattolia@gmail.com>
2024-06-14 17:03:43 -0500

    👽️ pruebas video sobre video

--- src/carealgo/carealgo.ts
+++ src/carealgo/carealgo.ts
@@ -7,6 +7,7 @@ import { dimsCamara } from '../constantes';
 let reloj = 0;
 const lienzo = document.createElement('canvas');
 const ctx = lienzo.getContext('2d') as CanvasRenderingContext2D;
+const videoTrump = document.getElementById('video') as HTMLVideoElement;
 const vision = await FilesetResolver.forVisionTasks();
 const marcadores = await FaceLandmarker.createFromOptions(vision, {
   baseOptions: { modelAssetPath: '/modelos/face_landmarker.task', delegate: 'GPU' },
@@ -14,13 +15,24 @@ const marcadores = await FaceLandmarker.createFromOptions(vision, {
   numFaces: 1,
   // outputSegmentationMasks: true
 });
-const contornoCara = FaceLandmarker.FACE_LANDMARKS_LIPS;
+const contornoBoca = FaceLandmarker.FACE_LANDMARKS_LIPS;
 const contornoOjoIzq = FaceLandmarker.FACE_LANDMARKS_LEFT_EYE;
 const contornoOjoDer = FaceLandmarker.FACE_LANDMARKS_RIGHT_EYE;
 const contornoTeselado = FaceLandmarker.FACE_LANDMARKS_TESSELATION;
+const contornoCara = [...contornoBoca, ...contornoOjoDer, ...contornoOjoIzq];
 const lienzo2 = new OffscreenCanvas(0, 0);
 const ctx2 = lienzo2.getContext('2d') as OffscreenCanvasRenderingContext2D;
-console.log(contornoCara);
+let reproduciendo = false;
+
+videoTrump.onplaying = (evento) => {
+  reproduciendo = true;
+};
+
+videoTrump.onpause = (evento) => {
+  reproduciendo = false;
+};
+
+//console.log(contornoCara);
 async function inicio() {
   const { ancho, alto } = dimsCamara();
   const escala = ancho / 2 / alto;
@@ -38,73 +50,130 @@ async function inicio() {
   color.addColorStop(0.5, 'lightblue');
   color.addColorStop(1, 'darkblue');
   // ctx.fillStyle = color;
+  const ojoManual = [33, 7, 163, 144, 145, 153, 154, 155, 133, 173, 157, 158, 159, 160, 161, 246];
   function espejitoEspejito(ahora: number) {
     ctx2.clearRect(0, 0, lienzo.width, lienzo.height);
+
     ctx.drawImage(camara, 0, 0);
 
     const poses = marcadores.detectForVideo(camara, ahora);
 
-    if (poses.faceLandmarks.length) {
-      poses.faceLandmarks.forEach((puntos) => {
-        console.log(puntos.length);
-        // pintor.drawConnectors(puntos, contornoCara, {
+    if (reproduciendo) {
+      const posesTrump = marcadores.detectForVideo(videoTrump, ahora + 1 / 1000);
+      ctx.fillStyle = 'black';
+      //ctx.clearRect(0, 0, lienzo.width, lienzo.height);
+      posesTrump.faceLandmarks.forEach((puntos) => {
+        ctx2.beginPath();
+        console.log(contornoOjoDer);
+        ojoManual.forEach((indice, i) => {
+          const punto = puntos[indice];
+
+          const x = punto.x * lienzo.width;
+          const y = punto.y * lienzo.height;
+
+          if (i === 0) {
+            ctx2.moveTo(x, y);
+            // ctx2.lineTo(x2, y2);
+          } else {
+            ctx2.lineTo(x, y);
+            // ctx2.lineTo(x2, y2);
+          }
+
+          // if (i === 0 || i === contornoOjoDer.length - 1 || i === contornoOjoDer.length - 2)
+          //   ctx2.fillText(`${start}`, x, y);
+        });
+
+        contornoBoca.forEach(({ start }, i) => {
+          const punto = puntos[start];
+          const x = punto.x * lienzo.width;
+          const y = punto.y * lienzo.height;
+          if (i === 0) {
+            ctx2.moveTo(x, y);
+          } else {
+            ctx2.lineTo(x, y);
+          }
+        });
+
+        contornoOjoIzq.forEach(({ start, end }, i) => {
+          const punto = puntos[start];
+          const puntoFinal = puntos[end];
+          const x = punto.x * lienzo.width;
+          const y = punto.y * lienzo.height;
+          //console.log(puntos);
+          if (i === 0) {
+            ctx2.moveTo(x, y);
+          } else {
+            ctx2.lineTo(x, y);
+          }
+        });
+
+        ctx2.closePath();
+        // console.log('---');
+        ctx2.fill();
+
+        // pintor.drawConnectors(puntos, contornoTeselado, {
         //   lineWidth: 1,
         //   color: color,
         //   fillColor: 'black',
         // });
-
-        ctx2.beginPath();
-
-        contornoCara.forEach(({ start }, i) => {
-          const punto = puntos[start];
-          const x = punto.x * lienzo.width;
-          const y = punto.y * lienzo.height;
-          if (i === 0) {
-            ctx2.moveTo(x, y);
-          } else {
-            ctx2.lineTo(x, y);
-          }
-        });
-
-        contornoOjoIzq.forEach(({ start }, i) => {
-          const punto = puntos[start];
-          const x = punto.x * lienzo.width;
-          const y = punto.y * lienzo.height;
-          if (i === 0) {
-            ctx2.moveTo(x, y);
-          } else {
-            ctx2.lineTo(x, y);
-          }
-        });
-
-        contornoOjoDer.forEach(({ start }, i) => {
-          const punto = puntos[start];
-          const x = punto.x * lienzo.width;
-          const y = punto.y * lienzo.height;
-          if (i === 0) {
-            ctx2.moveTo(x, y);
-          } else {
-            ctx2.lineTo(x, y);
-          }
-        });
-
-        /*        contornoTeselado.forEach(({ start }, i) => {
-          const punto = puntos[start];
-          const x = punto.x * lienzo.width;
-          const y = punto.y * lienzo.height;
-          if (i === 0) {
-            ctx2.moveTo(x, y);
-          } else {
-            ctx2.lineTo(x, y);
-          }
-        }); */
-
-        ctx2.fill();
       });
+
       ctx.save();
-      ctx.globalCompositeOperation = 'destination-in';
+      // ctx.globalCompositeOperation = 'destination-in';
+      ctx.globalCompositeOperation = 'source-atop';
       ctx.drawImage(lienzo2, 0, 0);
       ctx.restore();
+    }
+
+    const verCamara = true;
+
+    if (verCamara) {
+      if (poses.faceLandmarks.length) {
+        poses.faceLandmarks.forEach((puntos) => {
+          ctx2.beginPath();
+
+          contornoBoca.forEach(({ start }, i) => {
+            const punto = puntos[start];
+            const x = punto.x * lienzo.width;
+            const y = punto.y * lienzo.height;
+            if (i === 0) {
+              ctx2.moveTo(x, y);
+            } else {
+              ctx2.lineTo(x, y);
+            }
+          });
+
+          contornoOjoIzq.forEach(({ start, end }, i) => {
+            const punto = puntos[start];
+            const puntoFinal = puntos[end];
+            const x = punto.x * lienzo.width;
+            const y = punto.y * lienzo.height;
+            //console.log(puntos);
+            if (i === 0) {
+              ctx2.moveTo(x, y);
+            } else {
+              ctx2.lineTo(x, y);
+            }
+          });
+
+          contornoOjoDer.forEach(({ start }, i) => {
+            const punto = puntos[start];
+            const x = punto.x * lienzo.width;
+            const y = punto.y * lienzo.height;
+            if (i === 0) {
+              ctx2.moveTo(x, y);
+            } else {
+              ctx2.lineTo(x, y);
+            }
+          });
+
+          ctx2.fill();
+        });
+        ctx.save();
+        ctx.globalCompositeOperation = 'destination-in';
+        ctx.drawImage(lienzo2, 0, 0);
+        ctx.restore();
+      }
     }
 
     reloj = requestAnimationFrame(espejitoEspejito);
